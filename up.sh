f1a2b35dbc2d58813f46095b0ab2f2bdc4c7a745
alter <altereg0@users.com>
2020-03-06 09:14:58 +0300

    E

--- up.sh
+++ up.sh
@@ -1,14 +1,4 @@
-sudo mkdir -p ./volumes/1c-server/home
-sudo mkdir -p ./volumes/1c-server/logs
-
-sudo mkdir -p ./volumes/1c-pg/conf
-sudo mkdir -p ./volumes/1c-pg/data
-sudo mkdir -p ./volumes/1c-pg/log
-
-#sudo mkdir -p ./volumes/1c-ms/data
-#sudo mkdir -p ./volumes/1c-ms/logs
-
+#!/bin/bash
 sudo chown 999 -R ./volumes
 
-sudo docker-compose up -d
-#sudo docker-compose up -d
+sudo docker-compose --compatibility up -d
